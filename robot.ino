84e5fa7468c38dcd6c3ac53675fb024f8d2775e0
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-07-15 20:42:38 +0200

    Indicator led
    As a testbed for the correct installation of the arduino id
    I created an indicator light and uploaded to the arduino
    
    Note: I had problem selecting the correct port. The reason
    being that the usb board is only readable by root and dialout.
    
    By adding my self to the dialout group with the command below
    
    sudo usermod -G dialout -a <user>
    
    this problem was fixed.

--- robot.ino
+++ robot.ino
@@ -1,1 +1,18 @@
+const int indicator_pin = 13;
+
+int indicator_state = LOW;
+
+void setup() {
+  pinMode(indicator_pin, OUTPUT);
+}
+
+void loop() {
+  if (indicator_state == LOW) {
+    indicator_state = HIGH;
+  } else {
+    indicator_state = LOW;
+  }
+  digitalWrite(indicator_pin, indicator_state);
+  delay(500);
+}
 
