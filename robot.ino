7b0abee48cc4561d0f6e14f3a362a5562a2e8b72
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-07-15 21:01:11 +0200

    Both motors
    Both motors, in the correct order, are turning now.

--- robot.ino
+++ robot.ino
@@ -1,17 +1,22 @@
-const int left_motor_pin = 4;
-const int left_speed_pin = 5;
+const int left_direction_pin = 7;
+const int left_speed_pin = 6;
+const int right_direction_pin = 4;
+const int right_speed_pin = 5;
 
 int indicator_state = LOW;
 
 void setup() {
-  pinMode(left_motor_pin, OUTPUT);
+  pinMode(left_direction_pin, OUTPUT);
+  pinMode(right_direction_pin, OUTPUT);
 }
 
 void loop() {
   int value;
   for (value = 0; value < 255; value += 10) {
-    digitalWrite(left_motor_pin, LOW);
+    digitalWrite(left_direction_pin, LOW);
     analogWrite(left_speed_pin, value);
+    digitalWrite(right_direction_pin, LOW);
+    analogWrite(right_speed_pin, value);
     delay(100);
   }
 }
